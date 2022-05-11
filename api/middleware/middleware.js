6c56b78dc4a07125c5d41d2f927fc8305a6face3
Ricardorouge <ricky032929@gmail.com>
2022-05-11 16:10:49 -0400

    through put complete

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const Users = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
   const timestamp = new Date().toLocaleString()
@@ -7,22 +9,39 @@ function logger(req, res, next) {
   next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
-  console.log("validateUserId middleware");
-  next();
+  try{
+    const user = await Users.getById(req.params.id)
+    !user?
+      res.status(404).json({ message: "user not found"})
+    : 
+    req.user = user
+    next()
+  } catch(err){
+    res.status(500).end()
+
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
-  console.log("validateUser middleware");
-  next();
+  const {name} = req.body
+  !name?
+  res.status(400).json({
+    message: "missing required name field"
+  }):
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-  console.log("validatePost middleware");
-  next();
+ const {text} = req.body
+ !text?
+ res.status(400).json({
+  message: "missing required text field" 
+ }) :
+ next()
 }
 
 // do not forget to expose these functions to other modules
