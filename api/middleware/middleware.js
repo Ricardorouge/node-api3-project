b4761866c761da7905ce35518891cde1fc23ea39
Ricardorouge <ricky032929@gmail.com>
2022-05-11 14:29:57 -0400

    logger

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,29 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.url
+  console.log({timestamp,method,url})
+  next();
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validateUserId middleware");
+  next();
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validateUser middleware");
+  next();
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validatePost middleware");
+  next();
 }
 
 // do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validateUser, validatePost };
